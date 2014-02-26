997dc1872884f621824667ece1f31ec8b522ff9c
Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>
2014-02-26 09:33:58 +0100

    Fix build with clang 3.4
    clang 3.4 doesn't like inline functions that don't have
    an implementation available
    
    Change-Id: Ib38d8e0cc851fd0cf1b2cb615901a3d5c7f173ed
    Signed-off-by: Bernhard Rosenkränzer <Bernhard.Rosenkranzer@linaro.org>

--- exynos_omx/openmax/exynos_omx/component/video/dec/Exynos_OMX_Vdec.h
+++ exynos_omx/openmax/exynos_omx/component/video/dec/Exynos_OMX_Vdec.h
@@ -137,7 +137,10 @@ extern "C" {
 #endif
 
 int calc_plane(int width, int height);
-inline void Exynos_UpdateFrameSize(OMX_COMPONENTTYPE *pOMXComponent);
+#ifndef __clang__
+inline
+#endif
+	void Exynos_UpdateFrameSize(OMX_COMPONENTTYPE *pOMXComponent);
 OMX_BOOL Exynos_Check_BufferProcess_State(EXYNOS_OMX_BASECOMPONENT *pExynosComponent, OMX_U32 nPortIndex);
 OMX_ERRORTYPE Exynos_Input_CodecBufferToData(EXYNOS_OMX_BASECOMPONENT *pExynosComponent, OMX_PTR codecBuffer, EXYNOS_OMX_DATA *pData);
 OMX_ERRORTYPE Exynos_Output_CodecBufferToData(EXYNOS_OMX_BASECOMPONENT *pExynosComponent, OMX_PTR codecBuffer, EXYNOS_OMX_DATA *pData);
